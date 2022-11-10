f6aa51a454b57b8683c6f60df5e534235bd27652
ash730xo <109447309+ash730xo@users.noreply.github.com>
2022-11-10 15:11:42 -0500

    Add files via upload
    change the code around to see if it would work better/correctly, added more tests

--- segments.js
+++ segments.js
@@ -1,11 +1,13 @@
 /*
 https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection#Given_two_points_on_each_line
-
+Ashley Mills 
+11/10/2022
 */
 
 
 function segments(x1, x2, x3, x4, y1, y2, y3, y4){     
     /* 
+    Equation Logic 
     t = (( x1 - x3) (y3 - y4) - (y1 - y3) (x3 - x4)) 
                         / 
          (x1 - x2) (y3 - y4) - (y1 - y2) (x3 - x4)
@@ -17,33 +19,39 @@ function segments(x1, x2, x3, x4, y1, y2, y3, y4){
 
 
     //equation above split up
-    tNumerator = (x1 - x3)*(y3 - y4) - (y1 - y3)*(x3 - x4)
-    uNumerator = (x1 - x3)*(y1 - y2) - (y1 - y3)*(x1 - x2)
+    tNumerator = ((x1 - x3)*(y3 - y4)) - ((y1 - y3)*(x3 - x4))
+    tDenominator = ((x1 - x2)*(y3 - y4)) - ((y1 - y2)*(x3 - x4))
 
-    denominator = (x1 - x2)*(y3 - y4) - (y1 - y2)*(x3 - x4)
+    t = tNumerator/tDenominator
+
+    //equation above split up
+    uNumerator = ((x1 - x3)*(y1 - y2)) - ((y1 - y3)*(x1 - x2))
+    uDenominator = ((x1 - x2)*(y3 - y4)) - ((y1 - y2)*(x3 - x4))
 
     //equaiton put together
-    t = tNumerator/denominator
-    u = uNumerator/denominator
+    u = uNumerator/uDenominator
 
-    //points given point1x = x1, x2  point1y = y1, y2
-    
-    
-    //points given point2x = x3, x4  point2y = y3, y4   
-    point2x = (x3 + u*(x4 - x3))
-    point2y = (y3 + u*(y4 - y3))
-
-    if(denominator === 0){
+    // 0 < t <= 1 && 0 < u <= 1
+    if(tDenominator === 0){
         return null
-    } else if(0 < t <= 1 && 0 <u<=1){
-        point1x = (x1 + t*(x2 - x1))
-        point1y = (y1 + t*(y2 - y1))
-        return(point1x, point1y)
+    } else if (uDenominator === 0){
+        return null
+    } else if( 0 < t <= 1 && 0 < u <= 1){
+        //points given point1x = x1, x2  point1y = y1, y2
+        point1x = (x3 + u * (x3 - x4))
+        point1y = (y3 + u * (y4 - y3))
+
+        return point1x, point1y
     } else {
         console.log("IDK BRO")
     }
 }
 
-segments(0, 5, 5, 2, 0, 6, 6, 2)
-segments(8, 3, 6, 6, 5, 5, 7, 3)
-segments(0, 8, 0, 0, 0, 0, 0, 7)
+
+//TESTS
+segments(0, 5, 5, 2, 0, 6, 6, 2) //(5,6)
+segments(8, 3, 6, 6, 5, 5, 7, 3) //(6,5)
+segments(0, 8, 0, 0, 0, 0, 0, 7) //(0,0)
+segments(0, 0, 0, 0, 0, 0, 0, 0) //(null)
+segments(0, 3, 4, 5, 7, 2, 4 ,6) //(5,2)
+segments(0, 0, 0, 0, 1, 1, 1, 1) // (null)
